954769a319360976ee7db02c0de40cfe182cbac2
adi cohen <adimute@adis-mbp.home>
2019-10-21 23:56:21 +0300

    Updates

---
+++ precache-manifest.55e8eefbc31e05b959e689995a215bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7821f33cc6d90a56c2b43a0913a74306",
+    "url": "/adi/index.html"
+  },
+  {
+    "revision": "7c94df2a8d137d58bd3c",
+    "url": "/adi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "72827d99c83d5d74012c",
+    "url": "/adi/static/js/2.b7937580.chunk.js"
+  },
+  {
+    "revision": "7c94df2a8d137d58bd3c",
+    "url": "/adi/static/js/main.d3869b9d.chunk.js"
+  },
+  {
+    "revision": "f86eb9338f2f5371b258",
+    "url": "/adi/static/js/runtime-main.3a881597.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/adi/static/media/logo.25bf045c.svg"
+  }
+]);
